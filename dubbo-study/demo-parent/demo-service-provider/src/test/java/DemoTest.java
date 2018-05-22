122c516832a527a9138bce6cc13b963362734429
yikun.he <yunai.wang@eku001.com>
2018-05-22 17:00:25 +0800

    修改名字

--- dubbo-study/demo-parent/demo-service-provider/src/test/java/DemoTest.java
+++ dubbo-study/demo-parent/demo-service-provider/src/test/java/DemoTest.java
@@ -1,11 +1,9 @@
-import com.alibaba.dubbo.config.ApplicationConfig;
 import com.example.demo.ServiceCommonProviderApplication;
 import com.example.demo.service.IDemoService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
